6e5e76a825fc43db4b44f45c23713a74b80861da
Shi Li <shilicqupt@163.com>
2012-05-19 18:41:15 -0700

    judge a root user and rename the file

---
+++ am_i_root.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# am-i-root.sh:   Am I root or not?
+
+ROOT_UID=0   # Root has $UID 0.
+
+if [ "$UID" -eq "$ROOT_UID" ]  # Will the real "root" please stand up?
+then
+  echo "You are root."
+else
+  echo "You are just an ordinary user (but mom loves you just the same)."
+fi
+
+exit 0
+
